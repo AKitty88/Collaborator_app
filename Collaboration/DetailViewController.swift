e5fbc5c521deb5bc83d1a71506126e575081e9da
Generic <s5110592@g23p220z062001.student.ad.griffith.edu.au>
2018-05-01 16:41:06 +1000

    before add change

--- Collaboration/DetailViewController.swift
+++ Collaboration/DetailViewController.swift
@@ -64,7 +64,7 @@ class DetailViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 0        // todo
     }
 
     /*
