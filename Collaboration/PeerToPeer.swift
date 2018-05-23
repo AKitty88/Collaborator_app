877b90113733ee01f56ccaaee73273bbe4159bc3
Generic <s5110592@g23p220z062003.student.ad.griffith.edu.au>
2018-05-23 12:42:16 +1000

    commit changes

--- Collaboration/PeerToPeer.swift
+++ Collaboration/PeerToPeer.swift
@@ -18,13 +18,13 @@ class PeerToPeerManager: NSObject {
     static let serviceType = "task-kitty"
     var delegate: PeerToPeerManagerDelegate?
     
-    private let peerId = MCPeerID(displayName: "first person")//rand.nr
+    private let peerId = MCPeerID(displayName: "first person")      //rand.nr TODO
     private let serviceAdvertiser: MCNearbyServiceAdvertiser
     private let serviceBrowser: MCNearbyServiceBrowser
     public var session: MCSession
     
     override init() {
-        print("Initialising P2P Manager")
+        print("PtP - init")
         let service = PeerToPeerManager.serviceType
         session = MCSession(peer: peerId, securityIdentity: nil, encryptionPreference: .required)
         serviceAdvertiser = MCNearbyServiceAdvertiser(peer: peerId, discoveryInfo: [ peerId.displayName : UIDevice.current.name ], serviceType: service)
@@ -38,74 +38,78 @@ class PeerToPeerManager: NSObject {
     }
     
     deinit {
+        print("PtP - deinit")
         serviceAdvertiser.stopAdvertisingPeer()
         serviceBrowser.stopBrowsingForPeers()
     }
     
     func invite(peer: MCPeerID, timeout t: TimeInterval = 10) {
-        print("inviting \(peer.displayName)")        
+        print("PtP - invite \(peer.displayName)")
+        
         serviceBrowser.invitePeer(peer, to: session, withContext: nil, timeout: t)
         print("session: \(session) , session.connectedPeers: \(session.connectedPeers)")
     }
     
-    func send(data: Data) {
+    func send(data: Data) {             // peerToPeer.send(data: viewModel.json) -> put in CollaboratorTapped action (connect action)
+        print("PtP - send")
+        
         guard !session.connectedPeers.isEmpty else { return }
         do {
             try session.send(data, toPeers: session.connectedPeers, with: .reliable)
         } catch {
-            print("Error sending \(data.count) bytes: \(error)")
+            print("PtP - Error sending \(data.count) bytes: \(error)")
         }
     }
 }
 
 extension PeerToPeerManager: MCNearbyServiceAdvertiserDelegate {
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
-        print("didNotStartAdvertisingPeer: \(error)")
+        print("PtP - advertiser didNotStartAdvertisingPeer: \(error)")
     }
     
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
-        print("didReceiveInvitationFromPeer \(peerID)")
+        print("PtP - advertiser didReceiveInvitationFromPeer \(peerID)")
         invitationHandler(true, session)
     }
 }
 
 extension PeerToPeerManager: MCNearbyServiceBrowserDelegate {
     func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
-        print("didNotStartBrowsingForPeers: \(error)")
+        print("PtP - browser didNotStartBrowsingForPeers: \(error)")
     }
     
     func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
-        print("foundPeer: \(peerID) - \(info?.description ?? "<no info>")")
+        print("PtP - browser foundPeer: \(peerID) - \(info?.description ?? "<no info>")")
         invite(peer: peerID)
     }
     
     func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
-        print("lostPeer: \(peerID)")
+        print("PtP - browser lostPeer: \(peerID)")
     }
 }
 
 
 extension PeerToPeerManager: MCSessionDelegate {
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-        print("peer \(peerID) didChangeState: \(state.rawValue)")
+        print("PtP - session peer \(peerID) didChangeState: \(state.rawValue)")
     }
     
     func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-        print("didReceiveData: \(data)")
+        print("PtP - session didReceiveData: \(data)")
         //DispatchQueue.main.async {
             delegate?.manager(self, didReceive: data)
         //}
     }
     
     func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-        print("didReceiveStream")
+        print("PtP - session didReceiveStream")
     }
     
     func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
-        print("didStartReceivingResourceWithName")
+        print("PtP - session didStartReceivingResourceWithName")
     }
     
     func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
-        print("didFinishReceivingResourceWithName")
+        print("PtP - session didFinishReceivingResourceWithName")
     }
 }
